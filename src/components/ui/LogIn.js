2f2a98ede698045378a2598cdaa0a2f3d5711a9d
Zantos321 <eljoe1985@gmail.com>
2020-06-14 21:33:52 -0700

    commented out link

--- src/components/ui/LogIn.js
+++ src/components/ui/LogIn.js
@@ -1,6 +1,6 @@
 import React from "react";
 import classnames from "classnames";
-import { Link } from "react-router-dom";
+//import { Link } from "react-router-dom";
 import hash from "object-hash";
 import { v4 as getUuid } from "uuid";
 
