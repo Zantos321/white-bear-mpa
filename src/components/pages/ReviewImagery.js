dea27ab22afa6911fd7971ade9d7dea52d68fdd0
Zantos321 <eljoe1985@gmail.com>
2020-06-16 14:36:44 -0700

    added mockie

--- src/components/pages/ReviewImagery.js
+++ src/components/pages/ReviewImagery.js
@@ -2,29 +2,51 @@ import React from "react";
 import AppTemplate from "../ui/AppTemplate";
 import { Link } from "react-router-dom";
 import memoryCards from "../../mock-data/memory-cards";
+import axios from "axios";
 const memoryCard = memoryCards[2];
 
-export default function ReviewImagery() {
-   return (
-      <AppTemplate>
-         <div className="mb-5"></div>
-         <div className="mb-5">
-            <div className="card bg-primary">
-               <div className="card-body">{memoryCard.imagery}</div>
-            </div>
-         </div>
+class ReviewImagery extends React.Component {
+   constructor(props) {
+      super(props);
+      axios
+         .get("https://run.mocky.io/v3/41185b3f-1ca6-44c0-8338-f3bbf38058ca")
+         .then(function (response) {
+            // handle success
+            console.log(response);
+         })
+         .catch(function (error) {
+            // handle error
+            console.log(error);
+         })
+         .finally(function () {
+            // always executed
+         });
+   }
 
-         <Link to="/review-answer" className="btn btn-link mb-4">
-            Previous card
-         </Link>
-         <div className="float-right">
-            <Link
-               to="/review-answer"
-               className="btn btn-lrg btn-outline-primary"
-            >
-               Show answer
+   render() {
+      return (
+         <AppTemplate>
+            <div className="mb-5"></div>
+            <div className="mb-5">
+               <div className="card bg-primary">
+                  <div className="card-body">{memoryCard.imagery}</div>
+               </div>
+            </div>
+
+            <Link to="/review-answer" className="btn btn-link mb-4">
+               Previous card
             </Link>
-         </div>
-      </AppTemplate>
-   );
+            <div className="float-right">
+               <Link
+                  to="/review-answer"
+                  className="btn btn-lrg btn-outline-primary"
+               >
+                  Show answer
+               </Link>
+            </div>
+         </AppTemplate>
+      );
+   }
 }
+
+export default ReviewImagery;
